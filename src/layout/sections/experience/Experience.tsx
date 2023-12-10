9dae6c502ed1a68ba8da2fa098186aa62bf31e7d
ttony62 <1988anton62@gmail.com>
2023-12-10 21:04:55 +0300

    add Experience

---
+++ src/layout/sections/experience/Experience.tsx
@@ -0,0 +1,58 @@
+import styled from "styled-components"
+import { SectionHeading } from "../../../components/section-heading/SectionHeading"
+import { Icon } from "../../../components/icon/Icon"
+import { FlexWrapper } from "../../../components/flex-wrapper/FlexWrapper"
+
+export function Experience() {
+    
+    return(
+        <SectionExperience>
+            <SectionHeading fontSize="42px">
+                Work Experience
+            </SectionHeading>
+            
+                    <FlexWrapper justify="space-between">
+                        <div>
+                            <h3>Frontend Developer</h3>
+                            <FlexWrapper>
+                                <PlaceOfWork>
+                                    <Icon iconId="office" width="16" height="12" viewBox="0 0 16 12" />
+                                    <span>HAAPS</span>
+                                </PlaceOfWork>
+                                <City>
+                                    <Icon iconId="location" width="16" height="12" viewBox="0 0 16 12" />
+                                    <span>Ryazan</span>
+                                </City>
+                            </FlexWrapper>
+                        </div>
+                        <div>
+                            <ButtonLink href="#">Full Time</ButtonLink>  
+                            <Interval>
+                                <Icon iconId="calendar" width="16" height="12" viewBox="0 0 16 12" />
+                                <span>
+                                    Sep 2021 - Dec 2021
+                                </span>
+                            </Interval>
+                        </div>
+                    </FlexWrapper>
+                
+        </SectionExperience>
+    )
+}
+
+const SectionExperience = styled.section`
+    
+`
+const PlaceOfWork = styled.div`
+    display: flex;
+`
+const City = styled.div`
+    display: flex;
+`
+
+const Interval = styled.div`
+    display: flex;
+`
+const ButtonLink = styled.a`
+    
+`
