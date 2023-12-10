c62b26c4b3f0fe59b36539203bc8a18e424254c3
ttony62 <1988anton62@gmail.com>
2023-12-10 21:16:40 +0300

    feat About and Experience

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import './App.css';
+import { FlexWrapper } from './components/flex-wrapper/FlexWrapper';
 import { Header } from './layout/header/Header';
+import { About } from './layout/sections/about/About';
+import { Experience } from './layout/sections/experience/Experience';
 import { Main } from './layout/sections/main/Main';
+import imgDrawing from './assets/img/drawing.svg'
 
 function App() {
     return (
         <div className="App">
             <Header />
             <Main />
+            <FlexWrapper>
+            <div>
+                <About />
+                <Experience />
+            </div>
+            <svg width="621" height="1019" viewBox="0 0 621 1019" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <use xlinkHref={`${imgDrawing}#${"driwing"}`} />
+            </svg>
+            </FlexWrapper>
         </div>
     );
 }
