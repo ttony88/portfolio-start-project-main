fe46b7b6caa595d275a50880d9700cad702d1bbc
ttony62 <1988anton62@gmail.com>
2023-12-11 14:34:44 +0300

    add IntervalData

---
+++ src/components/interval-date/IntervalDate.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components"
+import { Icon } from "../icon/Icon"
+
+type IntervalDatePropsType = {
+    iconId?: string
+    interval: string
+}
+
+export function IntervalDate(props: IntervalDatePropsType) {
+    return(
+        <IntervalDateStyled>
+            <Icon iconId={props.iconId || "calendar"} width="16" height="12" viewBox="0 0 16 12" />
+            <span>
+                {props.interval}
+            </span>
+        </IntervalDateStyled>
+    )
+}
+
+const IntervalDateStyled = styled.div`
+    display: flex;
+`
