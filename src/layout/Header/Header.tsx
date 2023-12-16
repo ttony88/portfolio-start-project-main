7246586d104ab4775a8efcb2e023db4b21154e72
ttony62 <1988anton62@gmail.com>
2023-12-16 19:56:24 +0300

    fix Header

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -3,14 +3,13 @@ import { Icon } from "../../components/icon/Icon"
 import { NavBar } from "../../components/nav-bar/NavBar"
 import { NavLinksSocial } from "../../components/nav-link-social/NavLinksSocial"
 import { FlexWrapper } from "../../components/flex-wrapper/FlexWrapper"
+import { Logo } from "../../components/logo/Logo"
 
 export function Header(){
 
     return(
         <HeaderStyled>
-            <a href="#">
-                <Icon iconId="logo" width="97" height="59" viewBox="0 0 97 59"/>
-            </a>
+            <Logo />
             <FlexWrapper>
                 <NavBar />
                 <NavLinksSocial />
