0874906f7bde6064d7237ce080660b7583bfbb78
ttony62 <1988anton62@gmail.com>
2023-12-16 19:58:04 +0300

    add Logo

---
+++ src/components/logo/Logo.tsx
@@ -0,0 +1,10 @@
+import { Icon } from "../icon/Icon";
+
+export function Logo() {
+
+    return(
+        <a href="#">
+            <Icon iconId="logo" width="97" height="59" viewBox="0 0 97 59"/>
+        </a>
+    )
+}
