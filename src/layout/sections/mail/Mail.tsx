570e3c06f0a7189f2d50094aa49229fcb85e385e
ttony62 <1988anton62@gmail.com>
2023-12-16 19:55:54 +0300

    fix Mail

--- src/layout/sections/mail/Mail.tsx
+++ src/layout/sections/mail/Mail.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components"
 import { SectionTitle } from "../../../components/section-title/SectionTitle"
+import { MyMail } from "../../../components/my-mail/MyMail"
 
 export function Mail() {
 
@@ -8,9 +9,9 @@ export function Mail() {
             <SectionTitle fontSize="58px">
                 For any questions please mail me:
             </SectionTitle>
-            <a href="mailto:1988anton62@gmail.com">
+            <MyMail href="mailto:1988anton62@gmail.com">
                 1988anton62@gmail.com
-            </a>
+            </MyMail>
         </MailStyled>
     )
 }
