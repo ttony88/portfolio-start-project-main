d0f5d7c7509c3428a99b2b30f9e1dc45a27338ee
ttony62 <1988anton62@gmail.com>
2023-12-24 23:58:19 +0300

    fix SectionTitle

--- src/components/section-title/SectionTitle.tsx
+++ src/components/section-title/SectionTitle.tsx
@@ -4,14 +4,13 @@ import { thema } from "../../styles/Thema";
 type SectionTitlePropsType = {
     fontSize: string
     textAlign?: string
-    margin?: string
+    marginBottom?: string
 }
 
 export const SectionTitle = styled.h2<SectionTitlePropsType>`
     font-size: ${props => props.fontSize};
     text-align: ${props => props.textAlign || "start"};
-    margin: ${props => props.margin};
+    margin-bottom: ${props => props.marginBottom};
     color: ${thema.colors.titleFont || "0px"};
-    font-style: normal;
     font-weight: 700;
 `
