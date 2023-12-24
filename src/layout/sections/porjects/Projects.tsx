30c23ee3a26923a150ace09b47f5455e81010789
ttony62 <1988anton62@gmail.com>
2023-12-25 00:00:51 +0300

    add styles Projects

--- src/layout/sections/porjects/Projects.tsx
+++ src/layout/sections/porjects/Projects.tsx
@@ -1,8 +1,9 @@
 import styled from "styled-components"
-import { FlexWrapper } from "../../../components/flex-wrapper/FlexWrapper"
 import { Project } from "./project/Project"
 import { SectionTitle } from "../../../components/section-title/SectionTitle"
 import { SectionSubtitle } from "../../../components/section-subtitle/SectionSubtitle"
+import { GridWrapper } from "../../../components/grid-wrapper/GridWrapper"
+import { Container } from "../../../components/container/Container"
 
 type ProjectsPropsType = {
     projectsInfo: Array<{
@@ -19,29 +20,34 @@ export function Projects(props: ProjectsPropsType) {
 
     return(
         <ProjectsStyled>
-            <SectionTitle fontSize="48">
+            <Container>
+                <SectionTitle fontSize="48px" textAlign="center" marginBottom="50px">
                 Projects
             </SectionTitle>
             <SectionSubtitle>
                  Things I've built so far
             </SectionSubtitle>
-            <FlexWrapper wrap="wrap">
+            <GridWrapper columnsRepeat="3, 1fr" rowsRepeat="2" columnGap="35px" rowGap="60px">
                 {props.projectsInfo.map((i, index) => {
-                    return(
-                        <Project img={i.imgProject}
-                                 title={i.titleProject}
-                                 description={i.descriptionProject}
-                                 linkPreview={i.linkPreview}
-                                 linkCode={i.linkCode} 
-                                 techStack={i.techStack}
-                                 key={index}/>
-                    )
+                    return (
+                      <Project
+                        img={i.imgProject}
+                        title={i.titleProject}
+                        description={i.descriptionProject}
+                        linkPreview={i.linkPreview}
+                        linkCode={i.linkCode}
+                        techStack={i.techStack}
+                        key={index}
+                      />
+                    );
                 })}
-            </FlexWrapper>
+            </GridWrapper>
+            </Container>
+            
         </ProjectsStyled>
     )
 }
 
 const ProjectsStyled = styled.section`
-    
+    margin-bottom: 185px;
 `
