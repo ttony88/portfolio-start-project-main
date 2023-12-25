5e2abf8182ce6464b23c14820be172e1962518c2
ttony62 <1988anton62@gmail.com>
2023-12-25 20:06:32 +0300

    add styles in Mail

--- src/layout/sections/mail/Mail.tsx
+++ src/layout/sections/mail/Mail.tsx
@@ -1,12 +1,11 @@
 import styled from "styled-components"
 import { SectionTitle } from "../../../components/section-title/SectionTitle"
-import { MyMail } from "../../../components/my-mail/MyMail"
 
 export function Mail() {
 
     return(
         <MailStyled>
-            <SectionTitle fontSize="58px">
+            <SectionTitle fontSize="58px" textAlign="center" >
                 For any questions please mail me:
             </SectionTitle>
             <MyMail href="mailto:1988anton62@gmail.com">
@@ -17,5 +16,17 @@ export function Mail() {
 }
 
 const MailStyled = styled.section`
-    
+    margin-bottom: 200px;
+    text-align: center;
+    font-family: DM Sans;
+`
+const MyMail = styled.a`
+    background-image: linear-gradient(90deg, #13B0F5 -2.06%, #E70FAA 100%);
+    background-clip: text;
+    color: transparent;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    font-size: 58px;
+    font-weight: 700;
+    line-height: 2;
 `
