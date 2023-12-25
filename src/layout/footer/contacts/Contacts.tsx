166f1cc21a7aa21d26c2c11e92edd170457dc2b8
ttony62 <1988anton62@gmail.com>
2023-12-25 20:05:44 +0300

    fix Contacts

--- src/layout/footer/contacts/Contacts.tsx
+++ src/layout/footer/contacts/Contacts.tsx
@@ -1,9 +1,7 @@
+import styled from "styled-components";
 import { FlexWrapper } from "../../../components/flex-wrapper/FlexWrapper";
 import { Logo } from "../../../components/logo/Logo";
-import { MyMail } from "../../../components/my-mail/MyMail";
-import { MyPhone } from "../../../components/my-phone/MyPhone";
 import { NavLinksSocial } from "../../../components/nav-link-social/NavLinksSocial";
-import { Mail } from "../../sections/mail/Mail";
 
 export function Contacts() {
 
@@ -11,14 +9,21 @@ export function Contacts() {
         <FlexWrapper justify="space-between">
             <Logo />
             <FlexWrapper>
-                <MyPhone href="tel:+79511057033">
+                <PhoneLink href="tel:+79511057033">
                     +79511057033
-                </MyPhone>
-                <MyMail href="mailto:1988anton62@gmail.com">
+                </PhoneLink>
+                <MailLink href="mailto:1988anton62@gmail.com">
                     1988anton62@gmail.com
-                </MyMail>
+                </MailLink>
                 <NavLinksSocial />
             </FlexWrapper>
         </FlexWrapper>
     )
 }
+
+const MailLink = styled.a`
+    
+`
+const PhoneLink = styled.a`
+    
+`
