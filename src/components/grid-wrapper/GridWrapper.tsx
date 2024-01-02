0448da4e335063728258563df66bee0e4225ee84
ttony62 <1988anton62@gmail.com>
2024-01-02 17:00:23 +0300

    fix GridWrapper

--- src/components/grid-wrapper/GridWrapper.tsx
+++ src/components/grid-wrapper/GridWrapper.tsx
@@ -2,17 +2,17 @@ import styled from "styled-components"
 
 type GridWrapperPropsType = {
     columnsRepeat: string
-    rowsRepeat: string
     rowGap?: string
     columnGap?: string
+    justifyContent?: string
 }
 
 export const GridWrapper = styled.div<GridWrapperPropsType>`
     display: grid;
     grid-template-columns: repeat(${props => props.columnsRepeat});
-    grid-template-rows: repeat(${props => props.rowsRepeat});
     justify-content: center;
     justify-items: center;
     row-gap: ${props => props.rowGap};
     column-gap: ${props => props.columnGap};
+    justify-content: ${props => props.justifyContent};
 `
