b126cfd28278ff33009d1d129776a66b9f706b49
ttony62 <1988anton62@gmail.com>
2024-01-05 13:11:55 +0300

    add font in Common

---
+++ src/Common.ts
@@ -0,0 +1,18 @@
+import { thema } from "./styles/Thema"
+
+type FontPropsType = {
+    family?: string
+    weight?: number
+    color?: string
+    lineHeight?: number
+    Fmin: number
+    Fmax: number
+}
+
+export const font = ({family, weight, color, lineHeight, Fmin, Fmax}: FontPropsType) => `
+    font-family: ${family || 'Poppins'};
+    font-weight: ${weight || 400};
+    color: ${color || thema.colors.font};
+    line-height: ${lineHeight || 1.2};
+    font-size: calc( (100vw - 360px)/(1280 - 360) * (${Fmax} - ${Fmin}) + ${Fmin}px);
+`
